c0b430908bad92f42cd98ea90744f6819e6dbb44
zaragit <p080931@gmail.com>
2021-12-06 20:32:47 +0900

    update project name

--- android/app/src/main/java/com/diet_helper/MainActivity.java
+++ android/app/src/main/java/com/diet_helper/MainActivity.java
@@ -1,4 +1,4 @@
-package com.diet_helper2;
+package com.diet_helper;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "diet_helper2";
+    return "diet_helper";
   }
 }
