e97b5805d2394aeb4c367a63ab950d31e4905609
zaragit <p080931@gmail.com>
2022-01-16 17:43:48 +0900

    Created a new Expo app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import RootStack from './screens/RootStack';
-import {UserContextProvider} from './contexts/UserContext';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-function App() {
+export default function App() {
   return (
-    <UserContextProvider>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <RootStack />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </UserContextProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
